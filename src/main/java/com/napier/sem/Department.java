15233cd990ae826de92e64e738c337ce28e5045c
Khant Sithu Aung <45054709+caster082@users.noreply.github.com>
2020-01-21 15:42:35 +0630

    Adding Department class

---
+++ src/main/java/com/napier/sem/Department.java
@@ -0,0 +1,19 @@
+package com.napier.sem;
+
+public class Department {
+
+    public int dept_no;
+
+
+    /**
+     * Employee's current department
+     */
+    public String dept_name;
+
+    /**
+     * Employee's manager
+     */
+    public String manager;
+
+}
+
