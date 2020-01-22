b260f889be3b3466c79d4b417e74d8d4fe333b45
Khant Sithu Aung <45054709+caster082@users.noreply.github.com>
2020-01-22 10:36:57 +0630

    New class Department Created

--- src/main/java/com/napier/sem/Department.java
+++ src/main/java/com/napier/sem/Department.java
@@ -1,19 +1,7 @@
 package com.napier.sem;
 
 public class Department {
-
-    public int dept_no;
-
-
-    /**
-     * Employee's current department
-     */
+    public String dept_no;
     public String dept_name;
-
-    /**
-     * Employee's manager
-     */
-    public String manager;
-
+    public Employee manager;
 }
-
